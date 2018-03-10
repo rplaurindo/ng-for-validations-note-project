232003e2961feb6d0a58f2d43ed5bc6f38865f46
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-03-09 21:19:42 -0300

    add h3 for everyone

--- src/app/modules/examples.module.ts
+++ src/app/modules/examples.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
 
-// app imports
+// app component imports
 import { StructuralDirectiveDirective } from './examples/directives/structural-directive/structural-directive.directive';
 import { YellowBackgroundOnEventlistenerDirective } from './examples/directives/yellow-background-on-eventlistener/yellow-background-on-eventlistener.directive';
 import { LightgrayBackgroundDirective } from './examples/directives/lightgray-background/lightgray-background.directive';
@@ -16,14 +16,13 @@ import { ManipulatingDomComponent } from './examples/components/manipulating-dom
 
 import { DirectivesComponent } from './examples/components/directives/directives.component';
 
-import { InterpolationComponent } from './examples/components/interpolation/interpolation.component';
-import { InterpolationService } from './examples/services/interpolation/interpolation.service';
-
 import { EventComponent } from './examples/components/event/event.component';
 import { ComponentChildComponent } from './examples/components/component-child/component-child.component';
 
-// components and services
 import { DataBindingComponent } from './examples/components/data-binding/data-binding.component';
+
+import { InterpolationComponent } from './examples/components/interpolation/interpolation.component';
+import { EventSubscribeComponent } from './examples/components/event-subscribe/event-subscribe.component';
 
 @NgModule({
   imports: [
@@ -38,7 +37,8 @@ import { DataBindingComponent } from './examples/components/data-binding/data-bi
     ManipulatingDomComponent,
     DirectivesComponent,
     SafeNavigationComponent,
-    NgContentExampleComponent
+    NgContentExampleComponent,
+    EventSubscribeComponent
   ],
   declarations: [
     DataBindingComponent,
@@ -51,7 +51,8 @@ import { DataBindingComponent } from './examples/components/data-binding/data-bi
     NgContentExampleComponent,
     LightgrayBackgroundDirective,
     YellowBackgroundOnEventlistenerDirective,
-    StructuralDirectiveDirective
+    StructuralDirectiveDirective,
+    EventSubscribeComponent
   ],
   providers: []
 })
---
+++ src/app/modules/examples/components/event-subscribe/event-subscribe.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EventSubscribeService } from './../../services/event-subscribe/event-subscribe.service';
+
+@Component({
+  selector: 'app-event-subscribe',
+  templateUrl: './event-subscribe.component.html',
+  styleUrls: ['./event-subscribe.component.sass']
+})
+export class EventSubscribeComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
