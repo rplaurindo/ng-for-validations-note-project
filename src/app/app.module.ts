efbbd355dac4597af93867763481261f018c25af
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-03-13 23:46:05 -0300

    add AppRoutingModule to a app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // App imports
-import { ROUTING } from './app.routing';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ErrorsModule } from './modules/errors.module';
 import { ExamplesModule } from './modules/examples.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    ROUTING,
+    AppRoutingModule,
     BrowserModule,
     ExamplesModule,
     ErrorsModule
