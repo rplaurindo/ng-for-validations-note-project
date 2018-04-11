5d67e0944de09493db1cfb6eb1ef9c25638561b7
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-04-10 23:07:07 -0300

    removes comment

--- src/app/examples/components/cruds/cruds-routing.module.ts
+++ src/app/examples/components/cruds/cruds-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-// import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CrudComponent } from './cruds.component';
